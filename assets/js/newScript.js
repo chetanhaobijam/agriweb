435d1676426b77388cf98915434e5f25613598b3
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-06-21 16:37:28 +0530

    Finished styling news and gallery section

--- assets/js/newScript.js
+++ assets/js/newScript.js
@@ -41,3 +41,44 @@ const toggleMenuByBtn = () => {
 
 toggleMenuByIcon();
 toggleMenuByBtn();
+
+// Image Controls Functions
+const leftBtn = document.querySelector("#leftBtn i");
+const rightBtn = document.querySelector("#rightBtn i");
+const galleryImg = document.querySelector("#image img");
+
+const banners = [
+  "./assets/images/banner1.gif",
+  "./assets/images/banner2.jpg",
+  "./assets/images/banner3.gif",
+  "./assets/images/banner0.jpg"
+]
+
+const changeImgLeft = () => {
+  if(galleryImg.getAttribute("src") === banners[3]) {
+    galleryImg.setAttribute("src", banners[2]);
+  } else if(galleryImg.getAttribute("src") === banners[2]) {
+    galleryImg.setAttribute("src", banners[1]);
+  } else if(galleryImg.getAttribute("src") === banners[1]) {
+    galleryImg.setAttribute("src", banners[0]);
+  } else if(galleryImg.getAttribute("src") === banners[0]) {
+    galleryImg.setAttribute("src", banners[3]);
+  } 
+}
+
+const changeImgRight = () => {
+  if(galleryImg.getAttribute("src") === banners[3]) {
+    galleryImg.setAttribute("src", banners[0]);
+  } else if(galleryImg.getAttribute("src") === banners[0]) {
+    galleryImg.setAttribute("src", banners[1]);
+  } else if(galleryImg.getAttribute("src") === banners[1]) {
+    galleryImg.setAttribute("src", banners[2]);
+  } else if(galleryImg.getAttribute("src") === banners[2]) {
+    galleryImg.setAttribute("src", banners[3]);
+  } 
+}
+
+leftBtn.addEventListener("click", changeImgLeft);
+rightBtn.addEventListener("click", changeImgRight);
+
+setInterval(changeImgRight, 5000);
